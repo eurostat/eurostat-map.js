4abbc147789af1b58b84f333d4ce830214c1776a
jgaffuri <jgaf@protonmail.com>
2020-11-19 10:57:29 +0100

    structure and commment code

--- src/eurostat-map.js
+++ src/eurostat-map.js
@@ -231,7 +231,7 @@ export const map = function () {
 			//set precision
 			out.filters_["precision"] = out.precision_;
 			//select only required geo groups, depending on the specified nuts level
-			out.filters_["geoLevel"] = out.nutsLvl_ + ("" === "0" ? "country" : "nuts" + out.nutsLvl_);
+			out.filters_["geoLevel"] = out.nutsLvl_ + "" === "0" ? "country" : "nuts" + out.nutsLvl_;
 			//force filtering of euro-geo-aggregates
 			out.filters_["filterNonGeo"] = 1;
 
@@ -837,37 +837,9 @@ export const getColorLegend = function (colorFun) {
 
 
 
-//fill pattern style
 
-//build a fill pattern legend object { nd:"white", 0:"url(#pattern_0)", 1:"url(#pattern_1)", ... }
-export const getFillPatternLegend = function () {
-	return function (ecl) { return "url(#pattern_" + ecl + ")"; }
-}
+// indexing
 
-/**
- * make function which build fill patterns style
- * @param {*} opts 
- */
-export const getFillPatternDefinitionFun = function (opts) {
-	opts = opts || {};
-	opts.shape = opts.shape || "circle";
-	var ps = opts.patternSize || 5;
-	var smin = opts.minSize || 1;
-	var smax = opts.maxSize || 5.5;
-	opts.bckColor = opts.bckColor || "white";
-	opts.symbColor = opts.symbColor || "black";
-	return function (svg, clnb) {
-		for (var i = 0; i < clnb; i++) {
-			var si = smin + (smax - smin) * i / (clnb - 1);
-			var patt = svg.append("pattern").attr("id", "pattern_" + i).attr("x", "0").attr("y", "0").attr("width", ps).attr("height", ps).attr("patternUnits", "userSpaceOnUse");
-			patt.append("rect").attr("x", 0).attr("y", 0).attr("width", ps).attr("height", ps).style("stroke", "none").style("fill", opts.bckColor)
-			if (opts.shape == "square")
-				patt.append("rect").attr("x", 0).attr("y", 0).attr("width", si).attr("height", si).style("stroke", "none").style("fill", opts.symbColor)
-			else
-				patt.append("circle").attr("cx", ps * 0.5).attr("cy", ps * 0.5).attr("r", si * 0.5).style("stroke", "none").style("fill", opts.symbColor)
-		}
-	};
-};
 
 /**
  * Index JSONStat stat values by 'geo' code.
@@ -902,4 +874,47 @@ export const csvToIndex = function (csvData, geoCol, valueCol) {
 	}
 	return ind;
 };
+
+
+
+
+
+// fill pattern style
+
+
+/**
+ * Build a fill pattern legend object { nd:"white", 0:"url(#pattern_0)", 1:"url(#pattern_1)", ... }
+ */
+export const getFillPatternLegend = function () {
+	return function (ecl) { return "url(#pattern_" + ecl + ")"; }
+}
+
+
+/**
+ * Return a function which builds fill patterns style.
+ * The returned function has for arguments the SVG element where to use the fill pattern, and the number of classes.
+ * 
+ * @param {*} opts Various parameters on the fill pattern.
+ */
+export const getFillPatternDefinitionFun = function (opts) {
+	opts = opts || {};
+	opts.shape = opts.shape || "circle";
+	var ps = opts.patternSize || 5;
+	var smin = opts.minSize || 1;
+	var smax = opts.maxSize || 5.5;
+	opts.bckColor = opts.bckColor || "white";
+	opts.symbColor = opts.symbColor || "black";
+	return function (svg, clnb) {
+		for (var i = 0; i < clnb; i++) {
+			var si = smin + (smax - smin) * i / (clnb - 1);
+			var patt = svg.append("pattern").attr("id", "pattern_" + i).attr("x", "0").attr("y", "0").attr("width", ps).attr("height", ps).attr("patternUnits", "userSpaceOnUse");
+			patt.append("rect").attr("x", 0).attr("y", 0).attr("width", ps).attr("height", ps).style("stroke", "none").style("fill", opts.bckColor)
+			if (opts.shape == "square")
+				patt.append("rect").attr("x", 0).attr("y", 0).attr("width", si).attr("height", si).style("stroke", "none").style("fill", opts.symbColor)
+			else
+				patt.append("circle").attr("cx", ps * 0.5).attr("cy", ps * 0.5).attr("r", si * 0.5).style("stroke", "none").style("fill", opts.symbColor)
+		}
+	};
+};
+
 
