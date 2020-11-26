0ed1a11be1952ba0c004f1c1450493691f3d1a86
jgaffuri <jgaf@protonmail.com>
2020-11-26 12:00:10 +0100

    extract functions to new eurostat-map-util lib

--- src/eurostat-map-template.js
+++ src/eurostat-map-template.js
@@ -4,8 +4,10 @@ import { select, event } from "d3-selection";
 import { geoIdentity, geoPath } from "d3-geo";
 import { feature } from "topojson-client";
 import JSONstat from "jsonstat-toolkit";
-import { getEstatDataURL, flags, getURLParameterByName } from './lib/eurostat-base';
+import { getEstatDataURL, flags } from './lib/eurostat-base';
+import { getBBOXAsGeoJSON, csvToIndex, jsonstatToIndex } from './lib/eurostat-map-util';
 import * as tp from './lib/eurostat-tooltip';
+
 
 /**
  * Build an empty map template.
@@ -568,26 +570,6 @@ const _defaultPosition = {
 
 
 
-/**
- * Return a GeoJSON feature representing a bounding box, with multipoint geometry.
- * This bounding box is an array like the one in topojson bbox element.
- * [xmin,ymin,xmax,ymax]
- * This is useful for to call d3.fitSize([w, h], getTopoJSONExtentAsGeoJSON(topo.bbox)))
- * 
- * @param {*} bb The bounding box [xmin,ymin,xmax,ymax]. For topojson data, just give the topojson.bbox element. 
- */
-const getBBOXAsGeoJSON = function(bb) {
-	return {
-		type: "Feature",
-		geometry: {
-		  type: "MultiPoint",
-		  coordinates: [[bb[0], bb[1]], [bb[2], bb[3]]]
-		}
-	  };
-}
-
-
-//TODO extract to eurostat.js or new eurostat-map-util.js ?
 
 
 /**
@@ -622,6 +604,8 @@ const tooltipTextDefaultFunction = function (rg, map) {
 };
 
 
+
+
 /**
  * Retrieve some URL parameters, which could be then reused as map definition parameters.
  * This allow a quick map customisation by simply adding and changing some URL parameters.
@@ -633,49 +617,6 @@ export const getURLParameters = function () {
 		ps[p[i]] = getURLParameterByName(p[i]);
 	return ps;
 };
-
-
-
-
-
-
-// indexing
-
-
-/**
- * Index JSONStat stat values by 'geo' code.
- * Return a structure like: {geo:{value:0,status:""}}
- * 
- * @param {*} jsData The JSONStat data to index
- */
-export const jsonstatToIndex = function (jsData) {
-	const ind = {};
-	const geos = jsData.Dimension("geo").id;
-	for (let i = 0; i < geos.length; i++)
-		ind[geos[i]] = jsData.Data(i);
-	return ind;
-};
-
-
-
-/**
- * Index CSV stat values by 'geo' code.
- * Return a structure like: {geo:{value:0,status:""}}
- * 
- * @param {*} csvData The CSV data to index
- * @param {*} geoCol The name of the geo column in the CSV data
- * @param {*} valueCol The name of the statistical value column in the CSV file.
- */
-export const csvToIndex = function (csvData, geoCol, valueCol) {
-	const ind = {};
-	for (let i = 0; i < csvData.length; i++) {
-		const d = csvData[i];
-		const v = d[valueCol];
-		ind[d[geoCol]] = { value: isNaN(+v) ? v : +v, status: "" };
-	}
-	return ind;
-};
-
 
 
 
