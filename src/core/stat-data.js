115aa5de0aab797d1b932ad7310949316f329bab
jgaffuri <jgaf@protonmail.com>
2020-12-24 11:04:08 +0100

    new stat

--- src/core/stat-data.js
+++ src/core/stat-data.js
@@ -40,77 +40,62 @@ export const statData = function () {
 		const s = out.data_[nutsId];
 		if(s)
 			if(stat.value) { s.value = stat.value; s.status = stat.status; }
-			else s.value = stat;
+			else s.value = isNaN(+stat)?stat:+stat;
 		else
-			out.data_[nutsId] = stat.value? stat : {value:stat};
+			out.data_[nutsId] = stat.value? stat : {value:isNaN(+stat)?stat:+stat};
 	}
 
 
-	//information for stat data retrieved from Eurobase
+
+	/**
+	 * Retrieve stat data from remote data sources.
+	 * 
+	 * @param {*} nutsLvl 
+	 * @param {*} callback 
+	 */
+	out.updateStatDataB = function (nutsLvl, callback) {
+		if (out.csvDataSource_ == null) updateStatEurobase(nutsLvl, callback);
+		else updateStatCSV(callback);
+		return out;
+	}
+
+
+	//TODO decompose into Eurobase/jsonstat and CSV types?
+
+
+	/**
+	 * Eurobase/jsonstat data source
+	*/
 	out.datasetCode_ = "demo_r_d3dens";
 	out.filters_ = { lastTimePeriod: 1 };
 	out.precision_ = 2;
 	let jsonStatTime = undefined;
 
-	//information for stat data retrieved from CSV file
-	out.csvDataSource_ = null;
-
-	//TODO decompose CSV and jsonstat types?
 
 
-	/**
-	 * Return promise for Eurobase jsonstat data.
-	 */
-	out.getStatDataPromise = function(nutsLvl) {
-
-		//set precision
-		out.filters_["precision"] = out.precision_;
-		//select only required geo groups, depending on the specified nuts level
-		out.filters_["geoLevel"] = nutsLvl + "" === "0" ? "country" : "nuts" + nutsLvl;
-		//force filtering of euro-geo-aggregates
-		out.filters_["filterNonGeo"] = 1;
-
-		//retrieve stat data from Eurostat API
-		return json(getEstatDataURL(out.datasetCode_, out.filters_))
-	}
-
-
-	out.updateStatDataB = function (nutsLvl, callback) {
-
+	//for statistical data to retrieve from Eurostat API
+	//TODO document
+	const updateStatEurobase = function (nutsLvl, callback) {
 		//erase previous data
 		out.data_ = null;
 
-		if (out.csvDataSource_ == null) {
-			//for statistical data to retrieve from Eurostat API
-			out.getStatDataPromise(nutsLvl).then(
-				function (data___) {
+		out.getStatDataPromise(nutsLvl).then(
+			function (data___) {
 
-					//decode stat data
-					const jsd = JSONstat(data___);
-					//get time
-					jsonStatTime = JSONstat(data___).Dimension("time").id[0];
-					//index
-					out.data_ = jsonstatToIndex(jsd);
-					//TODO: use maybe https://github.com/badosa/JSON-stat/blob/master/utils/fromtable.md to build directly an index ?
+				//decode stat data
+				const jsd = JSONstat(data___);
+				//get time
+				jsonStatTime = JSONstat(data___).Dimension("time").id[0];
+				//index
+				out.data_ = jsonstatToIndex(jsd);
+				//TODO: use maybe https://github.com/badosa/JSON-stat/blob/master/utils/fromtable.md to build directly an index ?
 
-					callback();
-				});
-		} else {
-			//for statistical data to retrieve from custom CSV file
-
-			//retrieve csv data
-			csv(out.csvDataSource_.url).then(
-				function (data___) {
-					//decode stat data
-					out.data_ = csvToIndex(data___, out.csvDataSource_.geoCol, out.csvDataSource_.valueCol);
-					callback();
-				});
-		}
-		return out;
+				callback();
+		});
 	}
 
 	/**
-	 * Retrieve the time stamp of the dataset.
+	 * Return the time stamp of the jsonstat dataset.
 	*/
 	out.getTime = function () {
 		const t = out.filters_.time;
@@ -118,6 +103,31 @@ export const statData = function () {
 		if (!out.data_) return;
 		return jsonStatTime;
 	};
+
+
+
+
+
+
+	/**
+	 * CSV data source
+	*/
+	out.csvDataSource_ = null;
+
+	//for statistical data to retrieve from custom CSV file
+	//TODO document
+	const updateStatCSV = function (callback) {
+		//erase previous data
+		out.data_ = null;
+
+		//retrieve csv data
+		csv(out.csvDataSource_.url).then(
+			function (data___) {
+				//decode stat data
+				out.data_ = csvToIndex(data___, out.csvDataSource_.geoCol, out.csvDataSource_.valueCol);
+				callback();
+		});
+	}
 
     return out;
 }
