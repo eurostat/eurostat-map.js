b0d5e8dceb16fad79f67e0ae837f961e44293c97
jgaffuri <jgaf@protonmail.com>
2020-12-28 12:34:13 +0100

    insets

--- src/core/map-template.js
+++ src/core/map-template.js
@@ -107,9 +107,10 @@ export const mapTemplate = function (withCenterPoints) {
 		out[att.substring(0, att.length - 1)] = function(v) {
 			if (!arguments.length) return out[att];
 			out[att] = v;
-			//recursive call to inset components
-			for(const geo in out.insetTemplates_)
-				out.insetTemplates_[geo][att.substring(0, att.length - 1)](v);
+			//recursive call to inset components - TODO: only for some attributes, not all
+			//for(const geo in out.insetTemplates_) {
+				//out.insetTemplates_[geo][att.substring(0, att.length - 1)](v);
+			//}
 			return out;
 		};
 
