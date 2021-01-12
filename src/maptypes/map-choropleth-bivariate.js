d750887ae62531814482be7298dadebc9e8a7cca
jgaffuri <jgaf@protonmail.com>
2021-01-12 15:38:50 +0100

    a

--- src/maptypes/map-choropleth-bivariate.js
+++ src/maptypes/map-choropleth-bivariate.js
@@ -1,7 +1,6 @@
-import { select } from "d3-selection";
-//import { min, max } from "d3-array";
-import { scalePow, scaleQuantile, scaleQuantize, scaleThreshold } from "d3-scale";
-//import { interpolateYlOrBr } from "d3-scale-chromatic";
+import { color } from "d3-color";
+import { scalePow } from "d3-scale";
+import * as dsc from "d3-scale-chromatic";
 import * as smap from '../core/stat-map';
 //import * as lgch from '../legend/legend-choropleth';
 
@@ -21,23 +20,11 @@ export const map = function (config) {
 	//create map object to return, using the template
 	const out = smap.statMap(config);
 
-	/*/the number of classes
-	out.clnb_ = 7;
-	//the classification method
-	out.classifMethod_ = "quantile"; // or: equinter, threshold
-	//the threshold, when the classificatio method is 'threshold'
-	out.threshold_ = [0];
-	//when computed automatically, ensure the threshold are nice rounded values
-	out.makeClassifNice_ = true;
-	//the color function [0,1] -> color
-	out.colorFun_ = interpolateYlOrBr;
-	//a function returning the color from the class i
-	out.classToFillStyle_ = undefined;*/
 	//style for no data regions
 	out.noDataFillStyle_ = "darkgray";
-	/*/the classifier: a function which return a class number from a stat value.
+	//the classifier: a function which return a class number from a stat value.
 	out.classifier_ = undefined;
-*/
+
 
     /**
 	 * Definition of getters/setters for all previously defined attributes.
@@ -46,19 +33,15 @@ export const map = function (config) {
 	 *  - To get the attribute value, call the method without argument.
 	 *  - To set the attribute value, call the same method with the new value as single argument.
 	*/
-	/*["clnb_", "classifMethod_", "threshold_", "makeClassifNice_", "colorFun_", "classToFillStyle_", "noDataFillStyle_", "classifier_"]
+	["noDataFillStyle_", "classifier_"]
 		.forEach(function (att) {
 			out[att.substring(0, att.length - 1)] = function(v) { if (!arguments.length) return out[att]; out[att] = v; return out; };
-		});*/
-
-	//override of some special getters/setters
-	//out.colorFun = function (v) { if (!arguments.length) return out.colorFun_; out.colorFun_ = v; out.classToFillStyle_ = getColorLegend(out.colorFun_); return out; };
-	//out.threshold = function (v) { if (!arguments.length) return out.threshold_; out.threshold_ = v; out.clnb(v.length + 1); return out; };
+		});
 
 	//override attribute values with config values
-	//if(config) ["clnb","classifMethod","threshold","makeClassifNice","colorFun","classToFillStyle","noDataFillStyle"].forEach(function (key) {
-	//	if(config[key]!=undefined) out[key](config[key]);
-	//});
+	if(config) ["noDataFillStyle"].forEach(function (key) {
+		if(config[key]!=undefined) out[key](config[key]);
+	});
 
 	//@override
 	out.updateClassification = function () {
@@ -104,32 +87,26 @@ export const map = function (config) {
 	//@override
 	out.updateStyle = function () {
 
-		/*const colorScale = scaleQuantile()
-		.domain(out.statData("v2").getArray())
-		.range( [ "#ca0020", "#f4a582", "#92c5de", "#0571b0" ] );
+		//get stat data
+		const s1 = out.statData("v1");
+		const s2 = out.statData("v2");
 
-		var grayScale = scaleQuantile()
-		.domain(out.statData("v1").getArray())
-		.range( [ .15, .30, .60, .90, 1 ] );*/
+		//define bivariate scale
+		const scale = scaleBivariate(
+			[s1.getMin(), s1.getMax()], dsc.interpolateBlues, 0.15,
+			[s2.getMin(), s2.getMax()], dsc.interpolateReds, 0.25
+		);
 
-		const scale = scaleBivariate(out.statData("v1").getArray(), out.statData("v2").getArray());
-
+		//apply colors
 		out.svg().selectAll("path.nutsrg")
-			//.attr("fill-opacity", 1)
 			.transition().duration(out.transitionDuration())
 			.attr("fill", function (rg) {
-				const sv1 = out.statData("v2").getValue(rg.properties.id);
+				const sv1 = s1.getValue(rg.properties.id);
 				if(!sv1) return out.noDataFillStyle_;
-				const sv2 = out.statData("v2").getValue(rg.properties.id);
+				const sv2 = s2.getValue(rg.properties.id);
 				if(!sv2) return out.noDataFillStyle_;
 				return scale(sv1, sv2)
 			})
-			/*.transition().duration(out.transitionDuration())
-			.attr("fill-opacity", function (rg) {
-				const sv = out.statData("v1").getValue(rg.properties.id);
-				if(!sv) return out.noDataFillStyle_;
-				return grayScale(sv)
-			})*/
 		return out;
 	};
 
@@ -143,14 +120,25 @@ export const map = function (config) {
 }
 
 
-const scaleBivariate = function(dom1, dom2) {
+const scaleBivariate = function(domain1, colorFun1, exponant1, domain2, colorFun2, exponant2) {
 
-	var s1 = scalePow().exponent(0.15).domain( [Math.min(...dom1), Math.max(...dom1)] ).range([255,0])
-	var s2 = scalePow().exponent(0.25).domain( [Math.min(...dom2), Math.max(...dom2)] ).range([255,0])
+	//make color scales
+	const s1 = scalePow().exponent(exponant1).domain(domain1)
+	const s2 = scalePow().exponent(exponant2).domain(domain2)
 
 	return function(v1, v2) {
-	  var r = s1(v1);
-	  var b = s2(v2);
-  	  return "rgb("+r+","+((r+b)/2)+","+b+")";
+
+		//get rgba colors
+		const col1 = colorFun1(s1(v1)), col2 = colorFun2(s2(v2));
+
+		//get d3 color
+		const c1 = color(col1), c2 = color(col2)
+
+		//return middle color
+		return "rgba("
+			+ Math.round( 0.5*(c1.r+c2.r) ) + ","
+			+ Math.round( 0.5*(c1.g+c2.g) ) + ","
+			+ Math.round( 0.5*(c1.b+c2.b) ) + ","
+			+ Math.round( 0.5*(c1.opacity+c2.opacity) )+")";
 	}
   }
