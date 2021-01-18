70bd354f4bdb84893e4b0f71a525979e786dd01b
jgaffuri <jgaf@protonmail.com>
2021-01-18 10:16:50 +0100

    dynamic legend

--- src/legend/legend-stripe-composition.js
+++ src/legend/legend-stripe-composition.js
@@ -58,7 +58,7 @@ export const legend = function (map, config) {
         //draw legend elements for classes: rectangle + label
         let i=0;
         const scs = m.catColors();
-		for(let sc in scs) {
+		for(let code in scs) {
 
 			//the vertical position of the legend element
 			const y = out.boxPadding + (out.title? out.titleFontSize + out.boxPadding : 0) + i*(out.shapeHeight + out.shapePadding);
@@ -66,14 +66,32 @@ export const legend = function (map, config) {
 			//rectangle
 			lgg.append("rect").attr("x", out.boxPadding).attr("y", y)
 			.attr("width", out.shapeWidth).attr("height", out.shapeHeight)
-			.attr("fill", scs[sc])
+			.attr("fill", scs[code])
 			.attr("stroke", "black").attr("stroke-width", 0.5)
+			.on("mouseover", function () {
+				svgMap.selectAll("pattern").selectAll("rect[code='"+code+"']")
+					.style("fill", m.nutsrgSelFillSty())
+			})
+			.on("mouseout", function () {
+				const col = m.catColors()[code] || "lightgray";
+				svgMap.selectAll("pattern").selectAll("rect[code='"+code+"']")
+					.style("fill", col)
+			})
 
 			//label
 			lgg.append("text").attr("x", out.boxPadding+out.shapeWidth+out.labelOffset).attr("y", y+out.shapeHeight*0.5)
 			.attr("alignment-baseline", "middle")
-			.text( m.catLabels()[sc] || sc )
+			.text( m.catLabels()[code] || code )
 			.style("font-size", out.labelFontSize).style("font-family", out.fontFamily).style("fill", out.fontFill)
+			.on("mouseover", function () {
+				svgMap.selectAll("pattern").selectAll("rect[code='"+code+"']")
+					.style("fill", m.nutsrgSelFillSty())
+			})
+			.on("mouseout", function () {
+				const col = m.catColors()[code] || "lightgray";
+				svgMap.selectAll("pattern").selectAll("rect[code='"+code+"']")
+					.style("fill", col)
+			})
 
             i++;
         }
@@ -88,7 +106,7 @@ export const legend = function (map, config) {
 			.attr("fill", m.noDataFillStyle() )
 			.attr("stroke", "black").attr("stroke-width", 0.5)
 			.on("mouseover", function () {
-                //TODO
+				//TODO
 				/*const sel = svgMap.select("#g_nutsrg").selectAll("[ecl='nd']");
 				sel.style("fill", m.nutsrgSelFillSty());
 				sel.attr("fill___", function (d) { select(this).attr("fill"); });
