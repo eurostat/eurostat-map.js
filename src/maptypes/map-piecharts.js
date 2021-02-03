60f3a09feb509952b0dfbcccf972cbf05637b5c6
Joe Davies <jwd101@gmail.com>
2021-02-03 11:14:07 +0100

    added labelling and region total to tooltip

--- src/maptypes/map-piecharts.js
+++ src/maptypes/map-piecharts.js
@@ -1,5 +1,5 @@
-import { scaleSqrt, scaleQuantile, scaleQuantize, scaleThreshold } from "d3-scale";
-import { select, arc, pie, extent } from "d3";
+import { scaleSqrt } from "d3-scale";
+import { select, arc, pie, extent, sum } from "d3";
 import { interpolateOrRd } from "d3-scale-chromatic";
 import * as smap from '../core/stat-map';
 import * as lgpc from '../legend/legend-piecharts';
@@ -14,16 +14,16 @@ export const map = function (config) {
     //create map object to return, using the template
     const out = smap.statMap(config, true);
 
-    out.pieMinRadius = 5;
-    out.pieMaxRadius = 30;
+    out.pieMinRadius_ = 5;
+    out.pieMaxRadius_ = 15;
 
     // pie charts
     out.pieChartRadius_ = 10;
-    out.pieChartInnerRadius_ =0;
+    out.pieChartInnerRadius_ = 0;
 
-        //tooltip pie chart
-        out.tooltipPieRadius_ = 60;
-        out.tooltipPieInnerRadius_ = 0;
+    //tooltip pie chart
+    out.tooltipPieRadius_ = 40;
+    out.tooltipPieInnerRadius_ = 0;
 
     //colors - indexed by category code
     out.catColors_ = undefined;
@@ -37,6 +37,7 @@ export const map = function (config) {
     out.noDataFillStyle_ = "darkgray";
 
     out.sizeClassifier_ = null; //d3 scale for scaling pie sizes
+    out.statPie_ = null;
 
     /**
      * Definition of getters/setters for all previously defined attributes.
@@ -45,13 +46,13 @@ export const map = function (config) {
      *  - To get the attribute value, call the method without argument.
      *  - To set the attribute value, call the same method with the new value as single argument.
     */
-    ["catColors_", "catLabels_", "showOnlyWhenComplete_", "noDataFillStyle_", "pieMaxRadius_", "pieMinRadius_","pieChartRadius_", "pieChartInnerRadius_"]
+    ["catColors_", "catLabels_", "showOnlyWhenComplete_", "noDataFillStyle_", "pieMaxRadius_", "pieMinRadius_", "pieChartRadius_", "pieChartInnerRadius_"]
         .forEach(function (att) {
             out[att.substring(0, att.length - 1)] = function (v) { if (!arguments.length) return out[att]; out[att] = v; return out; };
         });
 
     //override attribute values with config values
-    if (config) ["catColors", "catLabels", "showOnlyWhenComplete", "noDataFillStyle", "pieMaxRadius", "pieMinRadius","pieChartRadius", "pieChartInnerRadius"].forEach(function (key) {
+    if (config) ["catColors", "catLabels", "showOnlyWhenComplete", "noDataFillStyle", "pieMaxRadius", "pieMinRadius", "pieChartRadius", "pieChartInnerRadius"].forEach(function (key) {
         if (config[key] != undefined) out[key](config[key]);
     });
 
@@ -59,16 +60,19 @@ export const map = function (config) {
     let statCodes = undefined;
 
     /**
-     * A function to define a stripe map easily, without repetition of information.
+     * A function to define a pie chart map easily, without repetition of information.
      * Only for eurobase data sources.
      * 
      * @param {*} stat A pattern for the stat data source
-     * @param {String} dim The dimension of the composition.
+     * @param {String} dim The dimension (defined in eurostat REST API) of the composition.
      * @param {Array} codes The category codes of the composition
      * @param {Array} labels Optional: The labels for the category codes
      * @param {Array} colors Optional: The colors for the category
      */
-    out.statComp = function (stat, dim, codes, labels, colors) {
+    out.statPie = function (stat, dim, codes, labels, colors) {
+
+        //set unitText of stat() from statPie()
+        // out.statData().unitText(stat.unitText);
 
         //add one dataset config for each category
         stat.filters = stat.filters || {};
@@ -97,6 +101,8 @@ export const map = function (config) {
 
         //set statCodes
         statCodes = codes;
+
+        //set 
 
         return out;
     }
@@ -135,31 +141,30 @@ export const map = function (config) {
         return comp;
     }
 
-    	//@override
-	out.updateClassification = function () {
+    //@override
+    out.updateClassification = function () {
 
-		//if not provided, get list of stat codes from the map stat data
-		if(!statCodes) {
-			//get list of stat codes.
-			statCodes = Object.keys(out.statData_);
-			//remove "default", if present
-			const index = statCodes.indexOf("default");
-			if (index > -1) statCodes.splice(index, 1);
+        //if not provided, get list of stat codes from the map stat data
+        if (!statCodes) {
+            //get list of stat codes.
+            statCodes = Object.keys(out.statData_);
+            //remove "default", if present
+            const index = statCodes.indexOf("default");
+            if (index > -1) statCodes.splice(index, 1);
         }
-        
+
 
         //define size scaling function
         let domain = getDatasetMaxMin();
-        console.log(domain)
-        //out.sizeClassifier_ = d3.scaleSqrt().domain(domain).range([out.pieMinRadius_,out.pieMaxRadius_])
+        out.sizeClassifier_ = scaleSqrt().domain(domain).range([out.pieMinRadius_, out.pieMaxRadius_])
 
-		return out;
+        return out;
     };
-    
+
 
     /**
      * @function getDatasetMaxMin
-     * @description gets the max and min totals of all datasets for each region. Used to define domain of pie size scaling function.
+     * @description gets the maximum and minimum total of all dimensions combined for each region. Used to define the domain of the pie size scaling function.
      * @returns [min,max]
      */
     function getDatasetMaxMin() {
@@ -167,31 +172,45 @@ export const map = function (config) {
         let totals = [];
         let sel = out.svg().select("#g_ps").selectAll("g.symbol").data();
 
-        sel.forEach((rg)=>{
+        sel.forEach((rg) => {
             let id = rg.properties.id;
-            let sum = 0;
-            //get stat value for each category of the region. Compute the sum.
-            for (let i = 0; i < statCodes.length; i++) {
-    
-                //retrieve code and stat value
-                const sc = statCodes[i]
-                const s = out.statData(sc).get(id);
-    
-                //case when some data is missing
-                if (!s || (s.value != 0 && !s.value) || isNaN(s.value)) {
-                    if (out.showOnlyWhenComplete()) return undefined;
-                    else continue;
-                }
-                sum += s.value;
+            let total = getRegionTotal(id);
+            if (total) {
+                totals.push(total);
             }
-            if (sum) {
-                totals.push(sum)
-            }   
+
         })
 
         let minmax = extent(totals);
         return minmax;
+    }
 
+    /**
+ * Get absolute total value of combined statistical values for a specific region. E.g total livestock
+ * @param {*} id nuts region id
+ */
+    const getRegionTotal = function (id) {
+        let sum = 0;
+        //get stat value for each category. Compute the sum.
+        for (let i = 0; i < statCodes.length; i++) {
+
+            //retrieve code and stat value
+            const sc = statCodes[i]
+            const s = out.statData(sc).get(id);
+
+            //case when some data is missing
+            if (!s || (s.value != 0 && !s.value) || isNaN(s.value)) {
+                if (out.showOnlyWhenComplete()) return undefined;
+                else continue;
+            }
+
+            sum += s.value;
+        }
+
+        //case when no data
+        if (sum == 0) return undefined;
+
+        return sum;
     }
 
     //@override
@@ -230,11 +249,12 @@ export const map = function (config) {
             };
 
             //create svg for pie chart
-            const r = out.pieChartRadius_, ir = out.pieChartInnerRadius_;
             let node = select("#pie_" + nutsid);
             const svg = node.append("g")
-            // .attr("viewBox", [-r, -r, 2 * r, 2 * r])
-            // .attr("width", 2 * r);
+
+            // define radius
+            const r = out.sizeClassifier_(getRegionTotal(nutsid));
+            const ir = out.pieChartInnerRadius_;
 
             //make pie chart. See https://observablehq.com/@d3/pie-chart
             const pie_ = pie().sort(null).value(d => d.value)
@@ -248,31 +268,6 @@ export const map = function (config) {
                 .attr("d", arc().innerRadius(ir).outerRadius(r))
 
         })
-
-
-        // data.forEach(region => {
-        //     let x = projection(region.geometry.coordinates)[0];
-        //     let y = projection(region.geometry.coordinates)[1];
-        //     let arcs = pie(region.data);
-        //     map
-        //       .append('g')
-        //       .attr("transform", "translate(" + x + "," + y + ")")
-        //       .attr('stroke', 'white')
-        //       .selectAll('path')
-        //       .data(arcs)
-        //       .join('path')
-        //       .attr('fill', d => sectorColor(d.data.name))
-        //       .attr(
-        //         'd',
-        //         d3
-        //           .arc()
-        //           .innerRadius(0)
-        //           .outerRadius(radiusFunction(region.total))
-        //       )
-        //       .append('title')
-        //       // title will not actually be visible
-        //       .text(d => `${d.data.name}: ${d.data.value}`);
-        //   });
     }
 
     //@override
@@ -282,44 +277,138 @@ export const map = function (config) {
 
 
     //specific tooltip text function
-	out.tooltipText_ =  function (rg, map) {
+    out.tooltipText_ = function (rg, map) {
 
-		//get tooltip
-		const tp = select("#tooltip_eurostat")
+        //get tooltip
+        const tp = select("#tooltip_eurostat")
 
-		//clear
-		tp.html("")
-		tp.selectAll("*").remove();
+        //clear
+        tp.html("")
+        tp.selectAll("*").remove();
 
-		//write region name
-		tp.append("div").html("<b>" + rg.properties.na + "</b><br>");
+        //write region name
+        tp.append("div").html("<b>" + rg.properties.na + "</b><br>");
 
-		//prepare data for pie chart
-		const data = []
-		const comp = getComposition(rg.properties.id);
-		for(const key in comp) data.push({ code:key, value:comp[key] })
+        //prepare data for pie chart
+        const data = []
+        const comp = getComposition(rg.properties.id);
+        for (const key in comp) data.push({ code: key, value: comp[key] })
 
-		//case of regions with no data
-		if(!data || data.length == 0) {
-			tp.append("div").html( out.noDataText() );
-			return;
-		};
+        //case of regions with no data
+        if (!data || data.length == 0) {
+            tp.append("div").html(out.noDataText());
+            return;
+        };
 
-		//create svg for pie chart
-		const r = out.tooltipPieRadius_, ir = out.tooltipPieInnerRadius_;
-		const svg = tp.append("svg").attr("viewBox", [-r, -r, 2*r, 2*r]).attr("width", 2*r);
+        //create svg for pie chart
+        // set the dimensions and margins of the graph
+        var width = 150,
+            height = 150,
+            margin = 30
 
-		//make pie chart. See https://observablehq.com/@d3/pie-chart
-		const pie_ = pie().sort(null).value(d => d.value)
-		svg.append("g")
-		.attr("stroke", "darkgray")
-		.selectAll("path")
-		.data( pie_(data) )
-		.join("path")
-		.attr("fill", d => { return out.catColors()[d.data.code] || "lightgray"} )
-		.attr("d", arc().innerRadius(ir).outerRadius(r) )
+        // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+        var radius = Math.min(width, height) / 2 - margin
+
+        // append the svg object to the div called 'my_dataviz'
+        var svg = tp
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        // const svg = tp.append("svg")
+        // .attr("viewBox", [-radius, -radius, 3 * radius, 3 * radius])
+        // .attr("width", 3 * radius);
+
+        let units = out.statData(statCodes[0]).unitText();
+
+        //make pie chart. See https://observablehq.com/@d3/pie-chart
+        const pie_ = pie().sort(null).value(d => d.value)
+        // svg.append("g")
+        //     .attr("stroke", "black")
+        //     .selectAll("path")
+        //     .data(pie_(data))
+        //     .join("path")
+        //     .attr("fill", d => { return out.catColors()[d.data.code] || "lightgray" })
+        //     .attr("d", arc().innerRadius(ir).outerRadius(r))
+        // const arcGenerator = arc().innerRadius(ir).outerRadius(r)
+        var innerArc = arc()
+            .innerRadius(radius * 0.5)         // This is the size of the donut hole
+            .outerRadius(radius * 0.8)
+
+        // Another arc that won't be drawn. Just for labels positioning
+        var outerArc = arc()
+            .innerRadius(radius * 0.9)
+            .outerRadius(radius * 0.9)
+
+        const pieData = pie_(data);
+        svg
+            .selectAll('allSlices')
+            .data(pieData)
+            .enter()
+            .append('path')
+            .attr('d', innerArc)
+            .attr('fill', d => { return out.catColors()[d.data.code] || "lightgray" })
+            .attr("stroke", "white")
+            .style("stroke-width", "1px")
+            .style("opacity", 0.7)
+
+        // Add the polylines between chart and labels:
+        svg
+            .selectAll('allPolylines')
+            .data(pieData)
+            .enter()
+            .append('polyline')
+            .attr("stroke", "black")
+            .style("fill", "none")
+            .attr("stroke-width", 1)
+            .attr('points', function (d) {
+                var posA = innerArc.centroid(d) // line insertion in the slice
+                var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+                var posC = outerArc.centroid(d); // Label position = almost the same as posB
+                var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+                posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+                return [posA, posB, posC]
+            })
+
+        // Add the polylines between chart and labels:
+        svg
+            .selectAll('allLabels')
+            .data(pieData)
+            .enter()
+            .append('text')
+            .text(function (d) { let n = (d.data.value * 100).toFixed(); if (!isNaN(n)) return n + "%"; })
+            .attr('transform', function (d) {
+                var pos = outerArc.centroid(d);
+                var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+                pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+                return 'translate(' + pos + ')';
+            })
+            .style('text-anchor', function (d) {
+                var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+                return (midangle < Math.PI ? 'start' : 'end')
+            })
+            .style("font-size", 12)
+
+        // Now add the annotation. Use the centroid method to get the best coordinates
+        // svg
+        //     .selectAll('mySlices')
+        //     .data(pieData)
+        //     .enter()
+        //     .append('text')
+        //     .text(function (d) { let num = d.data.value.toFixed(2); if(!isNaN(num)) return num; else return; })
+        //     .attr("transform", function (d) { return "translate(" + arcGenerator.centroid(d) + ")"; })
+        //     .style("text-anchor", "middle")
+        //     .style("font-size", 12)
+
+        // region total
+        tp.append("g").append("text").text((d) => {
+            if (rg)
+                return "Total: " + Math.floor(getRegionTotal(rg.properties.id)) + " " + units
+        })
     };
-    
+
 
     return out;
 }
