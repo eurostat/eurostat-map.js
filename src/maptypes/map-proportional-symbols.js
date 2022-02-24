b48732ba0abea9785c3c03684b6decd6adca4054
joewdavies <jwd101@gmail.com>
2022-02-24 12:07:50 +0100

    fix prop symbol stroke width for nutsbn

--- src/maptypes/map-proportional-symbols.js
+++ src/maptypes/map-proportional-symbols.js
@@ -261,15 +261,17 @@ export const map = function (config) {
 							return out.nutsbnStroke_[parseInt(lvl)] || "#777";
 						}
 					}
-
-
 				})
 				// nuts border stroke width
-				.style("stroke-width", function (rg) {
+				.style("stroke-width", function (bn) {
 					const lvl = select(this).attr("lvl");
-					const ecl = select(this).attr("ecl");
-					if (ecl && lvl !== "0") {
-						return out.nutsbnStrokeWidth_[parseInt(lvl)] || 0.2;
+					const sv = data.get(bn.properties.id);
+					if (!sv || !sv.value || sv.value==":" || !out.countriesToShow_.includes(bn.properties.id[0] + bn.properties.id[1])) {
+						return;
+					} else if (out.countriesToShow_.includes(bn.properties.id[0] + bn.properties.id[1])) {
+						if (lvl !== "0") {
+							return out.nutsbnStrokeWidth_[parseInt(lvl)] || "#777";
+						}
 					}
 				});
 			 }
